6725abedb9fa7146483319a2fa125a284b3d0d21
Saleh A <salehalakhras2000@gmail.com>
2022-10-05 01:12:59 +0300

    created the library array and the layout of the page

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+let library = [];
+const cardArea = document.querySelector('#area');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function Book(title, author, pages, read, price) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.price = price;
+}
 
-setupCounter(document.querySelector('#counter'))
+function addBook(title, author, pages, read, price) {
+  let newBook = new Book(title, author, pages, read, price);
+  library.push(newBook);
+}
+
+addBook("s", "s", "s", "s", "s");
+addBook("s", "s", "s", "s", "s");
+addBook("s", "s", "s", "s", "s");
+addBook("s", "s", "s", "s", "s");
+addBook("s", "s", "s", "s", "s");
+addBook("s", "s", "s", "s", "s");
+addBook("s", "s", "s", "s", "s");
+addBook("s", "s", "s", "s", "s");
+addBook("s", "s", "s", "s", "s");
+addBook("s", "s", "s", "s", "s");
+addBook("s", "s", "s", "s", "s");
+for(let i=0;i<library.length;i++) {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerHTML = `<p>${library[i].title}</p>
+  <p>${library[i].author}</p>
+  <p>${library[i].pages}</p>
+  <p>${library[i].price}</p>
+  <button id="btn-${i}" class="btn bg-gray-300 border-gray-400">Read</button>
+  <button id="del-btn-${i}"class="btn bg-red-600 border-red-700">Delete</button>`;
+  card.classList = 'card grid grid-cols-6 gap-x-20 justify-center items-center border-b border-gray-500 p-2';
+  cardArea.appendChild(card);
+}
+
