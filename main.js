962b9df72b02403aa2f969030a73bea5bf1d8a44
Saleh A <salehalakhras2000@gmail.com>
2022-10-05 23:46:15 +0300

    added delete btn functionality

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-let library = [];    let removeBtns = [];
-let readBtns = [];
-let i = 0;
+let library = [];
+let removeBtns = [];
+let bookArr = [];
 const cardArea = document.querySelector("#area");
+const titleRow = document.getElementById("titleRow");
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -9,7 +10,6 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
-
 
 function addBook(title, author, pages, read, price) {
   let newBook = new Book(title, author, pages, read);
@@ -21,30 +21,45 @@ Book.prototype.toggleRead = function () {
 };
 const addBtn = document.getElementById("addBook");
 
-addBtn.addEventListener("click", () => {
+function addBookForm() {
   const newTitle = document.getElementById("title").value;
   const newAuthor = document.getElementById("author").value;
   const newPages = document.getElementById("pages").value;
   const isRead = document.getElementById("checkbox").checked;
   addBook(newTitle, newAuthor, newPages, isRead);
-  const readBtns = [library.length];
-  for (i; i < library.length; i++) {
+}
+function displayLib() {
+  cardArea.innerHTML = "";
+  cardArea.appendChild(titleRow);
+  for (var i = 0; i < library.length; i++) {
     const card = document.createElement("div");
     card.innerHTML = `<p>${library[i].title}</p>
   <p>${library[i].author}</p>
   <p>${library[i].pages}</p>
-  <button id="btn-${i}" class="btn bg-gray-300 border-gray-400">${
+  <button id="btn-${i}" type="button" class="btn bg-gray-300 border-gray-400">${
       library[i].read ? "Read" : "Not Read"
     }</button>
   <button id="del-btn-${i}"class="btn bg-red-600 border-red-700">Delete</button>`;
-    card.classList =
-      `card card-${i} grid grid-cols-5 gap-x-20 justify-center items-center border-b border-gray-400 p-2`;
+    card.classList = `card card-${i} grid grid-cols-5 gap-x-20 justify-center items-center border-b border-gray-400 p-2`;
     cardArea.appendChild(card);
+    bookArr[i] = card;
     removeBtns[i] = document.getElementById(`del-btn-${i}`);
-    removeBtns[i].addEventListener('click',() => {
-      let removedCard = document.getElementById(`card-${i}`);
-      removedCard.remove();
+    removeBtns[i].addEventListener('click',()=>{
+      removeBook(event.target.id[event.target.id.length - 1]);
     });
   }
+}
+
+displayLib();
+
+function removeBook(id){
+  cardArea.removeChild(bookArr[id]);
+  library.splice(id,1);
+      displayLib();
+}
+
+addBtn.addEventListener("click", () => {
+  addBookForm();
+  displayLib();
 });
 
